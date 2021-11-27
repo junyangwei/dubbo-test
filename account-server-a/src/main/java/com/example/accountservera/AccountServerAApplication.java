bdf19255f0d8bf0fe6a4e0859f200aed41a97f45
junyangwei <799004508@qq.com>
2021-11-27 09:13:49 +0800

    初始化项目

---
+++ account-server-a/src/main/java/com/example/accountservera/AccountServerAApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountservera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServerAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServerAApplication.class, args);
+	}
+
+}
---
+++ account-server-b/src/main/java/com/example/accountserverb/AccountServerBApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountserverb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServerBApplication.class, args);
+	}
+
+}
