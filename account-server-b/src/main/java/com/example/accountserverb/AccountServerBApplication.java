ba24aa7456db2304165f6c9a99f91310bef9d969
junyangwei <799004508@qq.com>
2021-11-28 11:57:46 +0800

    为服务 B 添加配置属性，并且添加RPC调用服务 A 测试

--- account-server-b/src/main/java/com/example/accountserverb/AccountServerBApplication.java
+++ account-server-b/src/main/java/com/example/accountserverb/AccountServerBApplication.java
@@ -1,13 +1,34 @@
 package com.example.accountserverb;
 
+import com.example.demo.api.TestService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AccountServerBApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServerBApplication.class, args);
+	}
+
+	/**
+	 * 声明要调用的接口
+	 */
+	@DubboReference(version = "1.0.0")
+    private TestService testService;
+
+	/**
+	 * 测试调用方法
+	 */
+	@GetMapping("/test")
+	public void test() {
+		System.out.println("测试使用 Dubbo 进行 RPC 调用服务 A 的 TestService 接口实现...");
+		String message = testService.sayHi("dubbo");
+		System.out.println("调用响应结果: " + message);
 	}
 
 }
